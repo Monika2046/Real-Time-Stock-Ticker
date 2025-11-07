6e4b616f8569fa63b848b3db6b25938e040b6178
Monika Srinivasan <monikasrinivasan004@gmail.com>
2025-11-07 22:15:30 +0530

    Create App.js

---
+++ components/ components/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import StockTicker from './components/StockTicker';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <StockTicker />
+    </div>
+  );
+}
+
+export default App;
